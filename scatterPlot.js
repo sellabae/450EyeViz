c494194924ed4011a7cc7f022448063f3d3e29a2
sellabae <devsellabae@gmail.com>
2019-10-10 00:40:06 -0700

    Refactor the d3 codes

--- scatterPlot.js
+++ scatterPlot.js
@@ -1,15 +1,14 @@
 //global variables
-var mergedData;
-var linearScaleX, linearScaleY, linearScaleDuration, colorScale, timeScale
+var file, mergedData;
+var xScale, yScale, rScale, colorScale, timeScale
 var svg, svgDiv, svgHeight, svgWidth
-var file;
 
 document.addEventListener('DOMContentLoaded', function(){
     
     //setting global vars and drawing csv
     svgDiv = document.getElementById("svgDiv");
-    svgWidth = svgDiv.offsetWidth;
-    svgHeight = svgDiv.offsetHeight;
+    svgWidth = +svgDiv.offsetWidth;
+    svgHeight = +svgDiv.offsetHeight;
 
     svg = d3.select("#svgDiv")
         .append("svg")
@@ -17,12 +16,10 @@ document.addEventListener('DOMContentLoaded', function(){
         .attr("height", svgHeight)
         .attr("id", "drawnSvg");
 
-
     fetchCsvCallOthers();
-
 });
 
-//this fetches the csv, calls other functions
+// Fetches the csv, calls other functions
 function fetchCsvCallOthers(){
 
     var drawnSvg = document.getElementById("drawnSvg");
@@ -31,17 +28,17 @@ function fetchCsvCallOthers(){
     d3.selectAll("circle").remove();
 
     checkRadio();
+
     d3.csv(file)
     .then(function(data){
         //converting all rows to int
         data.forEach(function(d) {
-        d.time = +d.time;
-        d.duration = +d.duration;
-        d.x = +d.x;
-        d.y = +d.y;
-        d.avg_dilation = +d.avg_dilation;
+            d.time = +d.time;
+            d.duration = +d.duration;
+            d.x = +d.x;
+            d.y = +d.y;
+            d.avg_dilation = +d.avg_dilation;
         });
-        
         mergedData = data;
         setScales(mergedData);  
         drawCircles(mergedData);
@@ -50,64 +47,64 @@ function fetchCsvCallOthers(){
 
 //checks which radio button is checked
 function checkRadio(){
-
     if(document.getElementById("treeRadio").checked)
         file = "./data_preprocessed/merged_tree.csv";
     else
         file = "./data_preprocessed/merged_graph.csv";
-
 }
 
 //sets the scales for x, y coordinates, duration and avg_dilation
 function setScales(data){
+    const xValue = d => d.x;
+    const yValue = d => d.y;
+    const durationValue = d => d.duration;      // plot size
+    const pupilValue = d => d.avg_dilation;  // plot color
+    const timeValue = d => d.time;
 
-    //duration used for circle size, dilation used for color
+    var xMax = d3.max(data, xValue);
+    var xMin = d3.min(data, xValue);
+    console.log('x '+xMin+' : '+xMax);
+    var yMax = d3.max(data, yValue);
+    var yMin = d3.min(data, yValue);
+    console.log('y '+yMin+' : '+yMax);
+    var durationMax = d3.max(data, durationValue);
+    var durationMin = d3.min(data, durationValue);
+    console.log('duration '+durationMin+' : '+durationMax);
+    var pupilMax = d3.max(data, pupilValue);
+    var pupilMin = d3.min(data, pupilValue);
+    console.log('pupil '+pupilMin+' : '+pupilMax);
+    var timeMax = d3.max(data, timeValue);
+    var timeMin = d3.min(data, timeValue);
+    console.log('time '+timeMin+' : '+timeMax);
 
-    var xMax = d3.max(data, function(d) { return d.x; });
-    var xMin = d3.min(data, function(d) { return d.x; });
-    //console.log("xMax "+xMax + " xMin "+xMin);
+    xScale = d3.scaleLinear()
+        .domain([0, xMax])
+        .range([0 + 20,svgWidth-50])
+        .nice();
 
-    var yMax = d3.max(data, function(d) { return d.y; });
-    var yMin = d3.min(data, function(d) { return d.y; });
-    //console.log("yMax "+yMax + " yMin "+yMin);
+    yScale = d3.scaleLinear()
+        .domain([0, yMax])
+        .range([0 + 20,svgHeight-50])
+        .nice();
 
-    var maxDilation = d3.max(data, function(d) { return d.avg_dilation; });
-    var minDilation = d3.min(data, function(d) { return d.avg_dilation; });
-    //console.log("maxDilation "+maxDilation + " minDilation "+minDilation);
-
-    var maxDuration = d3.max(data, function(d) { return d.duration; });
-    var minDuration = d3.min(data, function(d) { return d.duration; });
-
-    linearScaleX = d3.scaleLinear()
-                    .domain([xMin, xMax])
-                    .range([0 + 20,svgWidth-50]);
-
-    linearScaleY = d3.scaleLinear()
-                    .domain([yMin, yMax])
-                    .range([0 + 20,svgHeight-50]);
-
-    linearScaleDuration = d3.scaleLinear()
-                    .domain([minDuration, maxDuration])
-                    .range([0,20]);
+    rScale = d3.scaleLinear()
+        .domain([100, durationMax])
+        .range([3, 30])
+        .nice();
 
     //var colorCodes = ["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598", "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"];
-
     /*colorScale = d3.scaleQuantile()
-                    .domain([minDilation,maxDilation])
+                    .domain([pupilMin,pupilMax])
                     .range(colorCodes);*/
-
     colorScale = d3.scaleLinear()
-                    .domain([minDilation, (minDilation+maxDilation)/2, maxDilation])
-                    .range(["#2c7bb6", "#ffff8c", "#d7191c"])
-                    .interpolate(d3.interpolateHcl);
-
-    var maxTime = d3.max(data, function(d) { return d.time});
-    var minTime = d3.min(data, function(d) { return d.time});
-    //console.log("maxTime "+maxTime + " minTime "+minTime);
+        .domain([pupilMin, (pupilMin+pupilMax)/2, pupilMax])
+        .range(["#2c7bb6", "#ffff8c", "#d7191c"])
+        .interpolate(d3.interpolateHcl);
 
     timeScale = d3.scaleLinear()
-                    .domain([minTime, maxTime])
-                    .range([0, 10]);
+        .domain([timeMin, timeMax])
+        .range([0, 10])
+        .nice();
 }
 
 //draws circle points
@@ -124,19 +121,10 @@ function drawCircles(data){
     svg.selectAll("circle")
         .data(data)
         .enter().append("circle")
-        .attr("cx", function(d){
-                return linearScaleX(d.x);
-            })
-        .attr("cy", function(d){
-                return linearScaleY(d.y);
-            })
-        .attr("r", function(d){
-            
-            return linearScaleDuration(d.duration);
-        })
-        .attr("fill", function(d){
-            return colorScale(d.avg_dilation);
-        })
+        .attr("cx", d => xScale(d.x))
+        .attr("cy", d => yScale(d.y))
+        .attr("r", d => rScale(d.duration))
+        .attr("fill", d => colorScale(d.avg_dilation))
         .attr("visibility","hidden")
         .on('mouseover', function(d, i) {
             tooltip.style("visibility", "visible");
@@ -152,7 +140,7 @@ function drawCircles(data){
         })
         .transition()
         .delay(function(d, i){
-            console.log(timeScale(i*d.time))
+            // console.log(timeScale(i*d.time));
             return timeScale(i*d.time);
         })
         .attr("visibility", "visible");
